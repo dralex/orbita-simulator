b2f4ca91d1b878c66372dd61a1b45e2f295fb928
Artem <vinogradovartem0412@gmail.com>
2023-07-26 18:25:16 +0300

    Update program.py

--- models/earth/pycontrol/program.py
+++ models/earth/pycontrol/program.py
@@ -31,7 +31,7 @@ import threading
 import atexit
 from collections import deque
 import gettext
-import posix_ipc
+import sysv_ipc
 
 _ = gettext.gettext
 
@@ -45,7 +45,7 @@ WORKER_LAUNCH_COMMAND = [sys.executable,
 WORKER_LAUNCH_TIMEOUT = 5.0
 
 DEFAULT_PARAMS_MAX_LINES = 1000
-DEFAULT_PARAMS_MAX_INIT_TIME = 2.0
+DEFAULT_PARAMS_MAX_INIT_TIME = 1.0
 DEFAULT_PARAMS_MAX_STEP_TIME = 0.1
 
 STDOUT_BUFFER_LINES = 4096
@@ -83,17 +83,43 @@ class ProgramError(ControlError):
         ControlError.__init__(self, msg)
         self.dump = dump
 
+
+import os
+import gettext
+
+DEFAULT_LANGUAGE = 'en'
+LANGUAGE_ORIGIN = 'sputnik'
+
+class Language:
+
+    Lang = DEFAULT_LANGUAGE
+    __tr = gettext.gettext
+
+    @classmethod
+    def set_lang(cls, lang):
+        if lang != DEFAULT_LANGUAGE:
+            cls.Lang = lang
+            t = gettext.translation(LANGUAGE_ORIGIN,
+                                    os.path.dirname(os.path.abspath(__file__)),
+                                    languages=[lang])
+            cls.__tr = t.gettext
+            t.install()
+
+    @classmethod
+    def get_tr(cls):
+        return cls.__tr
+
+
 class Program:
     def __init__(self,
                  runtime,
                  api_module_name,
                  code_lines,
-                 lang,
                  params_max_lines=DEFAULT_PARAMS_MAX_LINES,
                  params_max_init_time=DEFAULT_PARAMS_MAX_INIT_TIME,
                  params_max_step_time=DEFAULT_PARAMS_MAX_STEP_TIME):
         global _ # pylint: disable=W0603
-        _ = lang.get_tr()
+        _ = Language.get_tr()
 
         if len(code_lines) > params_max_lines:
             raise SecurityError(_('Program was not run because contains more than {} lines').format(params_max_lines)) # pylint: disable=C0301
@@ -142,40 +168,39 @@ class Program:
             self._stderr_reader = None
 
         if self._request_queue is not None:
-            self._request_queue.unlink()
-            self._request_queue.close()
+            self._request_queue.remove()
             self._request_queue = None
 
         if self._response_queue is not None:
-            self._response_queue.unlink()
-            self._response_queue.close()
+            self._response_queue.remove()
             self._response_queue = None
 
     def reset(self):
         self._destroy_worker()
 
-        self._request_queue = posix_ipc.MessageQueue(None,
-                                                     flags=posix_ipc.O_CREX,
-                                                     max_messages=1)
-        self._response_queue = posix_ipc.MessageQueue(None,
-                                                      flags=posix_ipc.O_CREX,
-                                                      max_messages=1)
+        self._request_queue = sysv_ipc.MessageQueue(None,
+                                                     flags=sysv_ipc.IPC_CREX)
+        self._response_queue = sysv_ipc.MessageQueue(None,
+                                                      flags=sysv_ipc.IPC_CREX)
 
         self._first_step = True
         self._deferred_response = None
 
         command = (WORKER_LAUNCH_COMMAND +
-                   [self._request_queue.name,
-                    self._response_queue.name])
+                   [str(self._request_queue.key),
+                    str(self._response_queue.key)])
+        
+        
         if self._api_module_name is not None:
+            
             command += [self._api_module_name]
-
+        print(command)
+        
         self._worker = subprocess.Popen(command,
                                         stdin=subprocess.PIPE,
                                         stdout=subprocess.PIPE,
-                                        stderr=subprocess.PIPE,
-                                        close_fds=True)
-
+                                        stderr=subprocess.PIPE)
+        
         def reader_task(pipe, queue_):
             while True:
                 line = pipe.readline(MAX_LINE_LENGTH)
@@ -190,35 +215,46 @@ class Program:
         self._stderr_reader = threading.Thread(target=reader_task,
                                                args=(self._worker.stderr,
                                                      self._stderr_buffer))
-
+        
         self._stdout_reader.daemon = True
         self._stderr_reader.daemon = True
-
+        
         self._stdout_reader.start()
         self._stderr_reader.start()
-
+        
         self._worker.stdin.write(self._code.encode('utf-8'))
         self._worker.stdin.close()
-
+        
+        time.sleep(0.2)
+        print(self._request_queue)
+        try:
+            print(self._request_queue.receive())
+        except:
+            print('not exist')
         if self._receive_from_worker(WORKER_LAUNCH_TIMEOUT) != b'READY':
+            
             self._destroy_worker()
             raise WorkerError('Child worker does not respond')
 
     def _send_to_worker(self, data, timeout):
         try:
-            self._response_queue.send(data, timeout=timeout)
-        except posix_ipc.BusyError:
+            self._response_queue.send(message = data)
+        except sysv_ipc.BusyError:
+            print('time error') 
             return False
 
         return True
 
     def _receive_from_worker(self, timeout):
-        try:
-            data = self._request_queue.receive(timeout)
-        except posix_ipc.BusyError:
-            return None
-        return data[0]
-
+        if self._request_queue.current_messages != 0:
+            try:
+                data = self._request_queue.receive()
+            except sysv_ipc.BusyError:
+                print('time error')
+                return None
+            return data[0]
+        else:
+            print('waiting for msg')
     def run(self):
         if self._worker is not None:
             # TODO: implement monotone time
@@ -240,6 +276,7 @@ class Program:
                 self._deferred_response = None
 
             while remaining_time >= 0:
+                print(1)
                 request = self._receive_from_worker(remaining_time)
                 if request is None:
                     break
